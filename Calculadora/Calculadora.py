c722bc94c5bbe5936aad6f785e77c7f2d3c161d7
RobGaudeoso <robertogaudeoso1@gmail.com>
2023-05-05 03:07:10 -0300

    add Calculadora

---
+++ Calculadora/Calculadora.py
@@ -0,0 +1,15 @@
+print("A Calculadora!")
+valor1 = input("Informe o primeiro valor: ")
+valor2 = input("Informe o segundo valor: ")
+# SOMA
+soma = float(valor1) + float(valor2)
+print("A soma é {}.".format(soma))
+# SUBTRAÇÃO
+subtracao = float(valor1) - float(valor2)
+print("A subtração é {}".format(subtracao))
+# DIVISÃO
+divisao = float(valor1) / float(valor2)
+print(f"A divisão é {divisao}")
+# MULTIPLICAÇÃO
+multiplicacao = float(valor1) * float(valor2)
+print(f"A multiplicação é {multiplicacao}")
