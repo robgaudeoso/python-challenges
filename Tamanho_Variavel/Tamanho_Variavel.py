2ccc2473855708cd22e2e5f1941658a123f751a9
RobGaudeoso <robertogaudeoso1@gmail.com>
2023-05-29 21:50:52 -0300

    add Tamanho_Variavel

---
+++ Tamanho_Variavel/Tamanho_Variavel.py
@@ -0,0 +1,13 @@
+#primeiro importamos o módulo sys
+import sys
+#depois criamos algumas variáveis de exemplo
+nome = "Bruce Wayne"
+idade = 30
+peso = 92.3
+lista = []
+
+#Vamos exibir em uma mensagem o nome da variável, o tipo (type) e o tamanho (getsizeof)
+print("A variável nome é do tipo {} e tem {} bytes".format(type(nome), sys.getsizeof(nome)))
+print("A variável idade é do tipo {} e tem {} bytes".format(type(idade), sys.getsizeof(idade)))
+print("A variável peso é do tipo {} e tem {} bytes".format(type(peso), sys.getsizeof(peso)))
+print("O objeto lista é do tipo {} e tem {} bytes".format(type(lista), sys.getsizeof(lista)))
