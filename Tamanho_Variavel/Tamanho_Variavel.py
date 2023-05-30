54c79b2f0e4cf8f82bae8d62975c16c5215a62b6
RobGaudeoso <robertogaudeoso1@gmail.com>
2023-05-29 22:02:17 -0300

    add tuple in Tamanho_Variavel

--- Tamanho_Variavel/Tamanho_Variavel.py
+++ Tamanho_Variavel/Tamanho_Variavel.py
@@ -5,9 +5,14 @@ nome = "Bruce Wayne"
 idade = 30
 peso = 92.3
 lista = []
+tupla = ()
+
 
 #Vamos exibir em uma mensagem o nome da variável, o tipo (type) e o tamanho (getsizeof)
 print("A variável nome é do tipo {} e tem {} bytes".format(type(nome), sys.getsizeof(nome)))
 print("A variável idade é do tipo {} e tem {} bytes".format(type(idade), sys.getsizeof(idade)))
 print("A variável peso é do tipo {} e tem {} bytes".format(type(peso), sys.getsizeof(peso)))
 print("O objeto lista é do tipo {} e tem {} bytes".format(type(lista), sys.getsizeof(lista)))
+print("O objeto tupla é do tipo {} e tem {} bytes".format(type(tupla), sys.getsizeof(tupla)))
+
+
